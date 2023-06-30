4a0b24d3675a2e6685b7bdc5b2e3d60509d9877e
clakr <clarktolosa@gmail.com>
2023-06-30 12:00:35 +0800

    added isr revalidation

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+export const revalidate = 5;
 
+const prisma = new PrismaClient();
 async function prismaExample() {
   const users = await prisma.user.findMany();
   return users;
