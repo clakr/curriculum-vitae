21ca3ea2a83ad98d38aeff908dd7a96605f126ab
clakr <clarktolosa@gmail.com>
2023-07-02 09:03:37 +0800

    about model w/ seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,6 +3,9 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
+  await prisma.basicInformation.deleteMany();
+  await prisma.about.deleteMany();
+
   await prisma.basicInformation.create({
     data: {
       firstName: "Clark Kenneth",
@@ -13,6 +16,20 @@ async function main() {
       email: "clarktolosa@gmail.com",
       phoneNumber: "(+63) 977 4499 113",
     },
+  });
+
+  await prisma.about.createMany({
+    data: [
+      {
+        desc: "I am a passionate Web Developer.",
+      },
+      {
+        desc: "Currently in love with Frontend Engineering with libraries and modern JavaScript frameworks with the likes of Tailwind CSS, React, and Next. Also interested in managing, interpreting, and visualizing data in the Backend and engineering the connection of both Frontend and Backend.",
+      },
+      {
+        desc: "Capable of Full Stack Web Development. Knowledgeable in modern technologies.",
+      },
+    ],
   });
 }
 
