065cb4a9353b8e4569e573fc122c4f47afb7e7b4
clakr <clarktolosa@gmail.com>
2023-07-12 15:55:22 +0800

    `<Author />` padding adjustment

--- components/Author.tsx
+++ components/Author.tsx
@@ -8,7 +8,7 @@ type Props = HTMLAttributes<HTMLElement> & {
 
 export default function Author({ info }: Props) {
   return (
-    <address className="hidden px-4 pt-4 not-italic dark:relative dark:after:absolute dark:after:inset-0 dark:after:-z-10 dark:after:bg-neutral-800 dark:after:opacity-90 laptop:block laptop:px-8 laptop:pt-8">
+    <address className="hidden px-6 pt-7 not-italic dark:relative dark:after:absolute dark:after:inset-0 dark:after:-z-10 dark:after:bg-neutral-800 dark:after:opacity-90 laptop:block">
       <h1 className="text-4xl font-extrabold">{formatToFullName(info)}</h1>
       <h2 className="text-3xl font-bold">{info?.position}</h2>
     </address>
