b02e57438519fc9ab1a1b706bb298fdd73866439
clakr <clarktolosa@gmail.com>
2024-07-14 08:32:51 +0800

    `<Header />` and other components stylign

--- src/components/Header.section.tsx
+++ src/components/Header.section.tsx
@@ -5,7 +5,8 @@ import {
   phoneNumber,
   about,
 } from "#src/information.json";
-import { useState } from "react";
+import { useState, type PropsWithChildren } from "react";
+import { twMerge } from "tailwind-merge";
 
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
@@ -15,28 +16,62 @@ export default function Header() {
   }
 
   return (
-    <header>
-      <h1>{name}</h1>
-      <h2>{occupation}</h2>
+    <header className="bg-gray-2 p-4 flex flex-col border-b border-gray-4 pb-2">
+      <h1 className="text-3xl font-bold tracking-tighter">{name}</h1>
+      <h2 className="text-2xl font-semibold">{occupation}</h2>
       {isOpen ? (
         <>
-          <section>
-            <h3>Contact</h3>
-            <address>{address.physical}</address>
-            <address>{address.email}</address>
-            <address>{phoneNumber}</address>
-          </section>
-          <ul>
-            <figcaption>About</figcaption>
+          <Section heading="Contact">
+            <Address>{address.physical}</Address>
+            <Address>{address.email}</Address>
+            <Address>{phoneNumber}</Address>
+          </Section>
+          <Section heading="About">
             {about.map((a, i) => (
-              <li key={i}>{a}</li>
+              <Paragraph key={i}>{a}</Paragraph>
             ))}
-          </ul>
+          </Section>
         </>
       ) : null}
-      <button type="button" onClick={handleOpen}>
-        {isOpen ? "close" : "open"}
+      <button
+        type="button"
+        className="grid place-content-center py-1.5"
+        onClick={handleOpen}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 256 256"
+          className={twMerge(
+            "w-6 h-6 transition-transform",
+            isOpen && "rotate-180"
+          )}
+        >
+          <path
+            fill="currentColor"
+            d="m216.49 104.49l-80 80a12 12 0 0 1-17 0l-80-80a12 12 0 0 1 17-17L128 159l71.51-71.52a12 12 0 0 1 17 17Z"
+          />
+        </svg>
       </button>
     </header>
   );
+}
+
+type SectionProps = PropsWithChildren<{
+  heading: string;
+}>;
+function Section({ children, heading }: SectionProps) {
+  return (
+    <section className="mt-3">
+      <h3 className="text-lg font-bold">{heading}</h3>
+      {children}
+    </section>
+  );
+}
+
+function Address({ children }: PropsWithChildren) {
+  return <address className="mt-1 not-italic">{children}</address>;
+}
+
+function Paragraph({ children }: PropsWithChildren) {
+  return <p className="mt-1">{children}</p>;
 }
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,23 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      // radix gray
+      gray: {
+        1: "color(display-p3 0.988 0.988 0.988)",
+        2: "color(display-p3 0.975 0.975 0.975)",
+        3: "color(display-p3 0.939 0.939 0.939)",
+        4: "color(display-p3 0.908 0.908 0.908)",
+      },
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Montserrat Variable", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
