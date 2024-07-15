38879d4ee6dd01f8be8caf5a40433df98c21c604
clakr <clarktolosa@gmail.com>
2024-07-15 10:56:49 +0800

    light mode initial design

--- src/components/Information/Address.tsx
+++ src/components/Information/Address.tsx
@@ -1,5 +1,5 @@
 import type { PropsWithChildren } from "react";
 
 export default function Address({ children }: PropsWithChildren) {
-  return <address className="mt-1 text-sm not-italic">{children}</address>;
+  return <address className="mt-1.5 text-sm not-italic">{children}</address>;
 }
--- src/components/Information/Paragraph.tsx
+++ src/components/Information/Paragraph.tsx
@@ -1,5 +1,5 @@
 import type { PropsWithChildren } from "react";
 
 export default function Paragraph({ children }: PropsWithChildren) {
-  return <p className="mt-1 text-sm">{children}</p>;
+  return <p className="mt-1.5 text-sm">{children}</p>;
 }
