c02c7e87f64fd0018c59c24255613b81fae2ccc3
clakr <clarktolosa@gmail.com>
2026-05-24 12:35:28 +0300

    chore: extracted components

---
+++ apps/web/src/components/sections/index.ts
@@ -0,0 +1,15 @@
+import Certifications from "@/components/sections/certifications.astro";
+import Education from "@/components/sections/education.astro";
+import Experience from "@/components/sections/experience.astro";
+import Languages from "@/components/sections/languages.astro";
+import Projects from "@/components/sections/projects.astro";
+import TechnicalSkills from "@/components/sections/technical-skills.astro";
+
+export {
+  Experience as ExperienceSection,
+  TechnicalSkills as TechnicalSkillsSection,
+  Education as EducationSection,
+  Projects as ProjectsSection,
+  Certifications as CertificationsSection,
+  Languages as LanguagesSection,
+};
