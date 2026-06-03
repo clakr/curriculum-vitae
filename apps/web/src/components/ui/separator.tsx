a748bf766743bf5948756acf58dce0375a684df4
clakr <clarktolosa@gmail.com>
2026-06-03 21:09:45 +0300

    feat: noise background

--- apps/web/src/components/ui/separator.tsx
+++ apps/web/src/components/ui/separator.tsx
@@ -15,7 +15,7 @@ function Separator({
       decorative={decorative}
       orientation={orientation}
       className={cn(
-        "shrink-0 bg-linear-to-r from-transparent via-mist-300 to-transparent data-horizontal:h-px data-horizontal:w-full data-vertical:w-px data-vertical:self-stretch",
+        "shrink-0 bg-linear-to-r from-transparent via-mist-400/50 to-transparent data-horizontal:h-px data-horizontal:w-full data-vertical:w-px data-vertical:self-stretch",
         className,
       )}
       {...props}
