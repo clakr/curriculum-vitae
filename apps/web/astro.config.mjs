b06dc401bbb2392d39ab66d1c99e999b8ff16bc4
clakr <clarktolosa@gmail.com>
2026-06-07 20:00:02 +0300

    feat(seo): implement an XML sitemap

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -3,11 +3,15 @@ import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
 
-  integrations: [react()],
+  integrations: [react(), sitemap()],
+  site: "https://cv.ckt.fyi/",
 });
+
